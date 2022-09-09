3042e0ff0f654270b67ea44063181f2c31c6ba7b
Fasan Michael <fasan.michael1@gmail.com>
2022-09-08 21:31:46 -0400

    prosper

--- index.js
+++ index.js
@@ -3,20 +3,44 @@
 
 
 
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
+const addToDoButton = document.getElementById("addToDo");
+const toDoContainer = document.getElementById("toDoContainer");
+const inputField = document.getElementById("inputField");
+const warning = document.getElementById("warning");
+warning.style.visibility = "hidden";
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
+addToDoButton.addEventListener("click", function () {
+  if (inputField === " ") {
+    warning.style.visibility = "visible";
+  } else {
+    warning.style.visibility = "hidden";
+  }
+  var paragraph = document.createElement("li");
+  paragraph.classList.add("paragraph-styling");
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener("click", function () {
+    paragraph.style.textDecoration = "line-through";
+  });
+  paragraph.addEventListener("dblclick", function () {
+    toDoContainer.removeChild(paragraph);
+  });
+});
+
+inputField.addEventListener("keyup", function (e) {
+  if (e.keyCode == 13) {
+    var paragraph = document.createElement("li");
+    paragraph.classList.add("paragraph-styling");
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-    })
-})
+    paragraph.addEventListener("click", function () {
+      paragraph.style.textDecoration = "line-through";
+    });
+    paragraph.addEventListener("dblclick", function () {
+      toDoContainer.removeChild(paragraph);
+    });
+  }
+});
+
