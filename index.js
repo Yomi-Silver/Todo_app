47bdf03f50b1ee33201080b9cbd3ccd9e23b8fad
Fasan Michael <fasan.michael1@gmail.com>
2022-09-17 21:32:22 -0400

    updated

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-
+var day = new Date();
+var time = day.getHours();
+if (time < 12) {
+  document.write("Good morning, dear MEC member!");
+} else if (time < 15) {
+  document.write("Good afternoon!");
+} else if (time == 16) {
+  alert("pack up, it's time to close for the day!");
+} else {
+  document.write("Have a good night rest!");
+}
 
 
 
@@ -9,38 +19,51 @@ const inputField = document.getElementById("inputField");
 const warning = document.getElementById("warning");
 warning.style.visibility = "hidden";
 
-addToDoButton.addEventListener("click", function () {
-  if (inputField === " ") {
-    warning.style.visibility = "visible";
-  } else {
-    warning.style.visibility = "hidden";
-  }
-  var paragraph = document.createElement("li");
-  paragraph.classList.add("paragraph-styling");
-  paragraph.innerText = inputField.value;
-  toDoContainer.appendChild(paragraph);
-  inputField.value = "";
-  paragraph.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-  paragraph.addEventListener("dblclick", function () {
-    toDoContainer.removeChild(paragraph);
-  });
-});
+ 
 
-inputField.addEventListener("keyup", function (e) {
-  if (e.keyCode == 13) {
-    var paragraph = document.createElement("li");
-    paragraph.classList.add("paragraph-styling");
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener("click", function () {
-      paragraph.style.textDecoration = "line-through";
-    });
-    paragraph.addEventListener("dblclick", function () {
-      toDoContainer.removeChild(paragraph);
-    });
-  }
-});
+  addToDoButton.addEventListener("click", function () {
+    if (inputField.value === "") {
+      warning.style.visibility = "visible";
+    } else {
+      warning.style.visibility = "hidden";
+
+      var paragraph = document.createElement("li");
+      paragraph.style.backgroundColor = "green";
+      paragraph.style.padding = "10px";
+      paragraph.style.borderRadius = "8px";
+      paragraph.classList.add("paragraph-styling");
+      paragraph.innerText = inputField.value;
+      toDoContainer.appendChild(paragraph);
+      inputField.value = "";
+      paragraph.addEventListener("click", function () {
+        paragraph.style.textDecoration = "line-through";
+      });
+      paragraph.addEventListener("dblclick", function () {
+        toDoContainer.removeChild(paragraph);
+      });
+    }
+  });
+
+  inputField.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) {
+      if (inputField.value === "") {
+        warning.style.visibility = "visible";
+      } else {
+        warning.style.visibility = "hidden";
+      var paragraph = document.createElement("li");
+      paragraph.style.backgroundColor = "green";
+      paragraph.style.padding = "10px";
+      paragraph.style.borderRadius = "8px";
+      paragraph.classList.add("paragraph-styling");
+      paragraph.innerText = inputField.value;
+      toDoContainer.appendChild(paragraph);
+      inputField.value = "";
+      paragraph.addEventListener("click", function () {
+        paragraph.style.textDecoration = "line-through";
+      });
+      paragraph.addEventListener("dblclick", function () {
+        toDoContainer.removeChild(paragraph);
+      });
+    }
+  }});
 
